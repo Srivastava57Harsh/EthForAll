8a301aa884ec72885b7a58afd4a0ab6d580c7b30
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-02-19 15:52:27 +0530

    :ambulance: hotfix related to upload

--- client/components/projects/new.tsx
+++ client/components/projects/new.tsx
@@ -122,7 +122,7 @@ const New = () => {
                 <label className="flex flex-col items-center justify-center w-2/12 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                   <div className="flex flex-col items-center justify-center pt-5 pb-6">
                     <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or drag and drop
+                      <span className="font-semibold">Click to upload</span>
                     </p>
                     <p className="text-xs text-gray-500 dark:text-gray-400">Upload Your files</p>
                   </div>
